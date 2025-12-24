d026e4cc938407a48872a23e7d4f8509fa5c21f3
kenjimattos <70449934+kenjimattos@users.noreply.github.com>
2025-12-24 06:56:23 -0300

    style: Responsividade do header

--- components/header.tsx
+++ components/header.tsx
@@ -1,26 +1,23 @@
 export const Header = () => {
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 flex justify-center py-[20px]">
+    <header className="fixed top-0 left-0 right-0 z-50 flex justify-center-safe"
+      style={{ padding: "clamp(10px, 2vw, 20px) clamp(10px, 4vw, 80px)" }}>
       <nav className="relative w-full max-w-[1200px] h-[60px]">
         <span
-          className="absolute top-[5px] left-0 text-[50px] leading-[1em] text-[#171717] font-normal"
+          className="absolute top-[5px] left-0 leading-[1em] text-[#171717] font-normal"
           style={{
             fontFamily: "var(--font-gravitas)",
             letterSpacing: "-0.11em",
             color: "var(--color-primary)",
+            fontSize: "clamp(32px, 6vw, 64px)",
           }}
         >
           knji
         </span>
-        <button className="absolute top-[4px] right-0 flex items-center justify-center text-white rounded-[2px] w-[150px] h-[51px]"
-          style={{ backgroundColor: "var(--color-primary)" }}>
-          <span
-            className="text-[24px] leading-[1em]"
-            style={{ fontFamily: "var(--font-gabarito)"
-            }}
-          >
+        <button className="absolute top-[4px] right-0">
+          
             Contact
-          </span>
+          
         </button>
       </nav>
     </header>
