6d68b2afe06f5afc572a71a7ca9f0c5c58511153
kenjimattos <70449934+kenjimattos@users.noreply.github.com>
2025-12-26 10:50:24 -0300

    fix: Smooth scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-scroll-behavior="smooth">
       <body
         className={`${gabarito.variable} ${gravitasOne.variable} antialiased`}
       >
