5eccffe4b2875ed77a3dec10c70189e26cadc20a
kenjimattos <70449934+kenjimattos@users.noreply.github.com>
2026-01-05 16:59:11 -0300

    feat: Remove importação desnecessária

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import { About } from "@/components/about";
 import { Work } from "@/components/work";
 import { TechStack } from "@/components/tech-stack";
 import { Contact } from "@/components/contact";
-import { Footer } from "@/components/footer";
 
 export default function Home() {
   return (
